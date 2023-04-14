76e427135c3f36edcc870ba86ea07387ac1d06ad
Park Se-Joon <spaniac@naver.com>
2023-04-14 16:56:35 +0900

    init with Telerik

---
+++ NpmuApartment_V2_1.0.0.0/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace NpmuApartment_V2_1._0._0._0
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+           this.InitializeComponent();
+        }
+    }
+}
